c1e94d3e497665672e7e312c297dc3e36437712b
arahmath <andirahmath8@gmail.com>
2023-10-04 05:08:04 -0700

    lol

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,9 +1,9 @@
 <?php
-class contoh1 extends CI_Controller
+class Contoh1 extends CI_Controller
 {
-    public funcion index()
+    public function index()
     {
-        echo "<h1>Perkenalkan</h1>";<br>
+        echo "<h1>Perkenalkan</h1>";
         echo "Nama Saya A.Rahmat Hidayat";
     }
 }
